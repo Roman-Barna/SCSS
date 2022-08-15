897b8890ea0af0cbc4dbd5f1524cabaf486dc896
Roman <bilkasnikers@gmail.com>
2022-08-15 15:54:44 +0300

    end video

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-window.onload = () => {
+
 
 let menuLink = document.querySelectorAll('.menu__link')
+let decor = document.querySelector('.header__content-decor')
+let decorInterval 
 
 // navbar
 
@@ -17,12 +19,29 @@ function resetNavActive() {
     menuLink.forEach(element => element.classList.remove('menu__link--active'))
 }
 
+function decorAnimate() {
+    let couterDecor = 15
+        decorInterval = setInterval(() => {
+            if (couterDecor <= 40) {
+                decor.style.setProperty('--topAfter', `${couterDecor++}%`)
+            } else {
+                couterDecor = 15
+            }
+        }, 30); 
+}
 let section = document.getElementsByTagName('section')
 
 window.addEventListener('scroll', () => {
     let windowScroll = window.scrollY
 
+    if (windowScroll == 0) {
+        decorAnimate()
+    } else {
+        clearInterval(decorInterval)
+    }
+
 for (const item of section) {
+   
     if (item.id == 'about' && windowScroll <= item.offsetTop) {
         resetNavActive()
         menuLink[0].classList.add('menu__link--active')
@@ -45,10 +64,71 @@ for (const item of section) {
 }
 })
 
+// decor
+
+decorAnimate()
+
+// download 
+
+let expertiseContentItem = document.querySelectorAll('.expertise__content-item')
+
+expertiseContentItem.forEach(element => {
+    let countDownload = 0
+    let downloadInterval
+    downloadInterval = setInterval(() => {
+        element.childNodes[1].innerHTML = `${countDownload}%`
+            if (countDownload < +element.getAttribute('data-width')) {
+                element.style.setProperty('--widthAfter', `${countDownload++}%`)
+            } else {
+                clearInterval(downloadInterval)
+            }
+        }, 30);
+})
+
+let static = document.querySelectorAll('.statistics__num')
+
+static.forEach(elem => {
+    let countDownloadNumbers = 0
+    let downloadIntervalNumbers
+    downloadIntervalNumbers = setInterval(() => {
+    if (countDownloadNumbers <= +elem.getAttribute('data-count')) {
+        elem.innerHTML = countDownloadNumbers++
+    }
+}, 30);
+})
+
 //
 
+let video = document.querySelector('.video__content')
+let playVideo = document.querySelector('.playVideo')
+let intervalEndVideo
+playVideo.addEventListener('click', () => {
+    video.volume = 0.1
+    if (video.paused) {
+        video.play()
+        playVideo.classList.remove('bi-google-play')
+        playVideo.classList.add('bi-pause-fill')
+        playVideo.style.fontSize = '70px'
+        intervalEndVideo = setInterval(() => {
+            if (video.ended) {
+                playVideo.classList.add('bi-google-play')
+                playVideo.classList.remove('bi-pause-fill')
+                playVideo.style.fontSize = '50px'
+            }
+        }, 100);
+    } else {
+        video.pause()
+        playVideo.classList.add('bi-google-play')
+        playVideo.classList.remove('bi-pause-fill')
+        playVideo.style.fontSize = '50px'
+        clearInterval(intervalEndVideo)
+    }
+    
+})
 
 
 
 
-}
+
+
+
